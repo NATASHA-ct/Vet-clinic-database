62fe5221736cd538dfd69dad5e711610f7215847
Natasha  Tatenda Chirombe <chirombenatashatatenda@gmail.com>
2022-09-21 22:04:04 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -75,22 +75,3 @@ SELECT MAX(escape_attempts) FROM animals WHERE neutered = true OR false;
 
 -- min and max weight of animals
 SELECT MIN(weight_kg), MAX(weight_kg) FROM animals;
-
--- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
-SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990 01-01' AND '2000-12-31' GROUP BY species;
-
--- 3rd day query using join 
-SELECT name FROM animals INNER JOIN owners ON animals.owner_id = owners.id WHERE owners.full_name = 'Melody Pond';
-
-SELECT animals.name FROM animals INNER JOIN species ON animals.species_id= species.id WHERE species.name = 'Pokemon';
-
-SELECT owners.full_name, name FROM owners LEFT JOIN animals ON animals.owner_id = owners.id;
-SELECT species.name, COUNT(animals.name) FROM animals INNER JOIN species ON animals.species_id = species_id GROUP BY species.name;
-
-SELECT animals.name FROM animals JOIN owners ON animals.owner_id = owners.id WHERE owners.full_name = 'Jennifer Orwell'; 
-
-SELECT animals.name FROM animals JOIN owners ON animals.owner_id = owners.id WHERE owners.full_name = 'Dean Winchester' AND animals.escape_attempts = 0; 
-
-SELECT owners.full_name, COUNT(animals.name) FROM owners LEFT JOIN animals ON owners.id = animals.owner_id GROUP BY owners.full_name ORDER BY COUNT(animals.name) DESC;
-
-

cbe65418ff975b7298bb779c34d2f8ad2ae6c53e
Natasha  Tatenda Chirombe <chirombenatashatatenda@gmail.com>
2022-09-21 15:36:55 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,14 @@
 /* Database schema to keep the structure of entire database. */
 
+/* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic;
+
 CREATE TABLE animals (
-    name varchar(100)
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(250) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT NOT NULL,
+    neutered BOOLEAN NOT NULL,
+    weight_kg DECIMAL NOT NULL,
+    PRIMARY KEY(id)
 );

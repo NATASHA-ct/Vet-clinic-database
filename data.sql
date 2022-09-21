1751db4f9181e92cca3b15444c553b29dc5eb861
Natasha  Tatenda Chirombe <chirombenatashatatenda@gmail.com>
2022-09-21 23:44:52 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -12,3 +12,21 @@ INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg) VALU
 ('Boarmon', '2005-06-07', 7, true, 20.4),
 ('Blossom', '1998-10-13', 3, true, 17),
 ('Ditto', '2022-05-14', 4, true, 22);
+
+-- inserting data into owners table
+INSERT INTO owners(full_name, age) VALUES('Sam Smith', 34), ('Jennifer Orwell', 19), ('Bob', 45), ('Melody Pond', 77),('Dean Winchester', 14),('Jodie Whittake', 38);
+
+-- inserting into species table
+INSERT INTO species(name) VALUES('Pokemon'),('Digimon');
+
+-- Modify inserted animals so it includes the species_id value
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+UPDATE animals SET species_id = 1 WHERE species_id IS NULL;
+
+
+-- Modify inserted animals to include owner information (owner_id):
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name = 'Gabumon' OR name = 'Pikachu';
+UPDATE animals SET owner_id = 3 WHERE name = 'Devimon' OR name = 'Plantmon';
+UPDATE animals SET owner_id = 4 WHERE name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom';
+UPDATE animals SET owner_id = 5 WHERE name = 'Angemon' OR name = 'Boarmon';
